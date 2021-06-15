0e03fdefa619ae6cac842adf5f6c230927db5452
furkanmtorun <furkanmtorun@gmail.com>
2021-06-15 13:31:44 +0000

    FIX: Session History bug via session state file

--- utils/session_states.py
+++ utils/session_states.py
@@ -44,6 +44,9 @@ def get(**kwargs):
             or
             # Streamlit >= 0.54.0
             (not hasattr(s, '_main_dg') and s.enqueue == ctx.enqueue)
+            or
+            # Streamlit >= 0.65.2
+            (not hasattr(s, '_main_dg') and s._uploaded_file_mgr == ctx.uploaded_file_mgr)
         ):
             this_session = s
 
